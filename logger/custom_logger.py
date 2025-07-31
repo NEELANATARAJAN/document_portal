19c0560fc73554ee3d552421eac0b2d52e5518c3
Natarajan Neela <bnneeladevi@gmail.com>
2025-07-31 16:33:45 +0530

    debugged custom_logger.py

--- logger/custom_logger.py
+++ logger/custom_logger.py
@@ -5,29 +5,16 @@ import structlog
 from datetime import datetime
 
 class CustomLogger:
-    def __init__(self,name:str):
+    def __init__(self,logs_dir='logs'):
         """
             init method of Custom Logger class creates log folder and 
             logfile (based on current timestamp) along with basic configuration
         """
-        self.log_path=os.path.join(os.getcwd(),"logs")
+        self.log_path=os.path.join(os.getcwd(),logs_dir)
         os.makedirs(self.log_path,exist_ok=True)
-        self.name=name
-
+        
         log_file=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
         self.log_file_path=os.path.join(self.log_path,log_file)
-
-        self.std_logger=logging.getLogger(self.name)
-
-        structlog.configure(
-            processors=[
-                structlog.processors.TimeStamper(fmt="iso", utc=True, key="timestamp"),
-                structlog.processors.add_log_level,
-                structlog.processors.JSONRenderer()
-            ],
-            logger_factory=structlog.stdlib.LoggerFactory(),
-            cache_logger_on_first_use=True
-        )
         
 
     def get_logger(self,name):
@@ -35,18 +22,31 @@ class CustomLogger:
             get_logger modeule will return the file name of the class that raises logs
         """
         logger_name = os.path.basename(name)
+        print(f"\n\nbasename: {logger_name}\n\n")
 
         # Configure logging for file + console
         file_handler = logging.FileHandler(self.log_file_path)
+        print(f"\n\nname={name},\nfile:{self.log_file_path}\n\n")
         file_handler.setLevel(logging.INFO)
-        formatter=structlog.stdlib.ProcessorFormatter(
-            processor=structlog.dev.ConsoleRenderer(),
+        # formatter=structlog.stdlib.ProcessorFormatter(
+        #     processor=structlog.dev.ConsoleRenderer(),
+        #     foreign_pre_chain=[
+        #         structlog.stdlib.add_logger_name,
+        #         structlog.stdlib.add_log_level,
+        #     ]
+        # )
+        # file_handler.setFormatter(logging.Formatter("%(message)s"))
+        file_handler.setFormatter(structlog.stdlib.ProcessorFormatter(
+            processor=structlog.processors.JSONRenderer(),
             foreign_pre_chain=[
                 structlog.stdlib.add_logger_name,
                 structlog.stdlib.add_log_level,
+                structlog.processors.TimeStamper(fmt="iso"),
+                structlog.processors.StackInfoRenderer(),
+                structlog.processors.format_exc_info,
+                structlog.processors.UnicodeDecoder(),
             ]
-        )
-        file_handler.setFormatter(formatter)
+        ))
 
         console_handler = logging.StreamHandler()
         console_handler.setLevel(logging.INFO)
@@ -65,6 +65,7 @@ class CustomLogger:
             processors=[
                 structlog.processors.TimeStamper(fmt="iso", utc=True, key="timestamp"),
                 structlog.processors.add_log_level,
+                structlog.processors.EventRenamer(to="event"),
                 structlog.processors.JSONRenderer()
             ],
             logger_factory=structlog.stdlib.LoggerFactory(),
@@ -75,9 +76,8 @@ class CustomLogger:
 
 
 if __name__=="__main__":
-    logger=CustomLogger(__file__)
-    logger=logging.getLogger(__file__)
-    print(__file__)
+    logger=CustomLogger().get_logger(__file__)
+    print(f"fileinmain:{__file__}\n\n")
     logger.debug("This is a debug message.", user_id="123", feature="login")
     logger.info("Custom Logger initialized...")
     logger.info("Moving on to exception handling...")
