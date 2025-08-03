57f03e167034aae5aabdec8157bf930d153c806a
Natarajan Neela <bnneeladevi@gmail.com>
2025-08-03 15:30:48 +0530

    Updated config loader model loader and models.py for pydantic models

--- exception/custom_exception.py
+++ exception/custom_exception.py
@@ -1,47 +1,31 @@
-import os
-import sys
 import traceback
-from logger.custom_logger import CustomLogger
-
-# Custom Logger object is created to store any exception on to a log file
-
+import sys
+from logger.custom_logger1 import CustomLogger
 logger=CustomLogger().get_logger(__file__)
 
 class DocumentPortalException(Exception):
-    def __init__(self, error_message, error_details:sys):
-        """
-            init method initializes exception info, file name, line number, error message and 
-            full traceback details
-        """
-        _,_,exc_tb=error_details.exc_info()
+    """Custom exception for Document Portal"""
+    def __init__(self,error_message,error_details):
+        _,_,exc_tb= error_details.exc_info()
         self.file_name=exc_tb.tb_frame.f_code.co_filename
-        self.line_no=exc_tb.tb_lineno
+        self.lineno=exc_tb.tb_lineno
         self.error_message=str(error_message)
-        self.traceback_string=''.join(traceback.format_exception(*error_details.exc_info()))
-
+        self.traceback_str = ''.join(traceback.format_exception(*error_details.exc_info())) 
+        
     def __str__(self):
-        """
-            str dunder method formats the exception in readable format
-        """
-        print(f"""
-        Error in [{self.file_name}] at line [{self.line_no}]
+       return f"""
+        Error in [{self.file_name}] at line [{self.lineno}]
         Message: {self.error_message}
         Traceback:
-        {self.traceback_string}
-        """)
-        return f"""
-        Error in [{self.file_name}] at line [{self.line_no}]
-        Message: {self.error_message}
-        Traceback:
-        {self.traceback_string}
-        """ 
-
-if __name__=="__main__":
+        {self.traceback_str}
+        """
+    
+if __name__ == "__main__":
     try:
-        a=1/0
+        # Simulate an error
+        a = 1 / 0
         print(a)
     except Exception as e:
         app_exc=DocumentPortalException(e, sys)
-        print(f"app_exc:\n\n{app_exc}\n\n")
-        logger.exception(app_exc)
-        # raise app_exc
+        logger.error(app_exc)
+        raise app_exc
---
+++ logger/custom_logger1.py
@@ -0,0 +1,123 @@
+import os
+import sys
+import logging
+import structlog
+from datetime import datetime
+
+class CustomLogger:
+    def __init__(self,logs_dir='logs'):
+        """
+            init method of Custom Logger class creates log folder and 
+            logfile (based on current timestamp) along with basic configuration
+        """
+        self.log_path=os.path.join(os.getcwd(),logs_dir)
+        os.makedirs(self.log_path,exist_ok=True)
+        
+        log_file=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+        self.log_file_path=os.path.join(self.log_path,log_file)
+        
+
+    def get_logger(self,name):
+        """
+            get_logger modeule will return the file name of the class that raises logs
+        """
+        logger_name = os.path.basename(name)
+        print(f"\n\nbasename: {logger_name}\n\n")
+
+        # Configure logging for file + console
+        file_handler = logging.FileHandler(self.log_file_path)
+        print(f"\n\nname={name},\nfile:{self.log_file_path}\n\n")
+        # file_handler.setLevel(logging.INFO)
+        # formatter=structlog.stdlib.ProcessorFormatter(
+        #     processor=structlog.dev.ConsoleRenderer(),
+        #     foreign_pre_chain=[
+        #         structlog.stdlib.add_logger_name,
+        #         structlog.stdlib.add_log_level,
+        #     ]
+        # )
+        # file_handler.setFormatter(logging.Formatter("%(message)s"))
+        file_handler.setFormatter(structlog.stdlib.ProcessorFormatter(
+            processor=structlog.processors.JSONRenderer(),
+            foreign_pre_chain=[
+                structlog.stdlib.add_logger_name,
+                structlog.stdlib.add_log_level,
+                structlog.processors.TimeStamper(fmt="iso"),
+                structlog.processors.StackInfoRenderer(),
+                structlog.processors.format_exc_info,
+                structlog.processors.UnicodeDecoder(),
+            ]
+        ))
+
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+        console_handler.setFormatter(logging.Formatter("%(message)s"))
+
+        logging.basicConfig(
+            format="%(message)s",
+            level=logging.INFO,
+            # handlers=[file_handler]
+            handlers=[console_handler, file_handler]
+        )
+        # root_logger=logging.getLogger()
+        # root_logger.addHandler(file_handler)
+        # root_logger.setLevel(logging.INFO)
+        structlog.configure(
+            processors=[
+                structlog.processors.TimeStamper(fmt="iso", utc=True, key="timestamp"),
+                structlog.processors.add_log_level,
+                structlog.processors.EventRenamer(to="event"),
+                structlog.processors.StackInfoRenderer(),
+                structlog.processors.format_exc_info,
+                structlog.processors.UnicodeDecoder(),
+                structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
+            ],
+            wrapper_class=structlog.stdlib.BoundLogger,
+            logger_factory=structlog.stdlib.LoggerFactory(),
+            cache_logger_on_first_use=True
+        )
+        self.logger = structlog.get_logger(logger_name)
+
+        return self.logger
+
+    def info(self, event, **kwargs):
+        """Logs an informational message."""
+        self.logger.info(event, **kwargs)
+
+    def warning(self, event, **kwargs):
+        """Logs a warning message."""
+        self.logger.warning(event, **kwargs)
+
+    def error(self, event, **kwargs):
+        """Logs an error message."""
+        self.logger.error(event, **kwargs)
+
+    def debug(self, event, **kwargs):
+        """Logs a debug message."""
+        self.logger.debug(event, **kwargs)
+
+    def critical(self, event, **kwargs):
+        """Logs a critical message."""
+        self.logger.critical(event, **kwargs)
+
+    def exception(self, event, **kwargs):
+        """
+        Logs an error message with exception info.
+        This method automatically sets exc_info=True.
+        """
+        self.logger.exception(event, **kwargs)
+
+if __name__=="__main__":
+    logger=CustomLogger().get_logger(__file__)
+    print(f"fileinmain:{__file__}\n\n")
+    logger.debug("This is a debug message.", user_id="123", feature="login")
+    logger.info("Custom Logger initialized...")
+    logger.info("Moving on to exception handling...")
+    # logger.warning("Disk space is low.", free_space_gb=5)
+    # logger.error("Failed to connect to database.", db_host="localhost", port=5432)
+    # logger.critical("System is shutting down due to critical error.")
+    # try:
+    #     1/0
+    # except ZeroDivisionError as e:
+    #     logger.exception("An unexpected error occured during execution...")
+    
+    # print(f"\nLog messages written to {__file__}...")
