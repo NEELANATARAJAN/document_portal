16c31270992ea864eebd1eb090d7229147558d4c
“Neela <bnneeladevi@gmail.com>
2025-09-01 13:51:52 +0530

    Code updated and ready for deployment

--- setup.py
+++ setup.py
@@ -13,19 +13,18 @@ setup(
     name="hw_document_portal",
     author="neela natarajan",
     version="0.1",
-    description="LLM powered intelligent document analysis and comparison system",
+    description="An intelligent document analysis and comparison system powered by LLMs",
     long_description=Path("README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
-    packages=find_packages(exclude=["test*", "examples*"]),
+    packages=find_packages(exclude=["tests*", "examples*"]),
     include_package_data=True,
-    install_requires=parse_requirements("requirement.txt"),
-    extras_require = {
+    install_requires=parse_requirements("requirements.txt"),
+    extras_require={
         "dev": ["pytest", "pylint", "ipykernel"]
     },
     classifiers=[
         "Programming Language :: Python :: 3.10",
-        "license :: OSI Approved :: MIT License",
-    ]
+        "License :: OSI Approved :: MIT License",
+    ],
     python_requires=">=3.10",
-    
 )
